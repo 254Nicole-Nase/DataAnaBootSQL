372789246ddf939968f589af079055606ea4a003
Nicole Nasengo <nasengonicole@gmail.com>
2024-10-08 13:05:00 +0300

    inner joins recap, outer joins (left & right), self joins and joining multiple tables together thru inner joins

--- joins in sql.sql
+++ joins in sql.sql
@@ -21,3 +21,33 @@ from employee_demographics as dem
 inner join employee_salary as sal
 	on dem.employee_id = sal.employee_id
 ;
+-- Outer Joins
+-- left(outer)join take everything from left table and only return matches from right tables
+-- exact opposite for the right(outer)join
+select *
+from employee_demographics as dem
+left join employee_salary as sal
+	on dem.employee_id = sal.employee_id
+;
+-- Self join where you tie the table to itself
+select  emp1.employee_id as emp_santa,
+emp1.first_name as first_name_santa,
+emp1.last_name as last_name_santa,
+emp2.employee_id as emp_name_emp,
+emp2.first_name as first_name_emp,
+emp2.last_name as last_name_emp
+from employee_salary emp1
+join employee_salary emp2
+	on emp1.employee_id + 1 = emp2.employee_id
+;
+-- Joining multiple tables together eg tie emp_dem to pd through emp_sal
+select *
+from employee_demographics as dem
+inner join employee_salary as sal
+	on dem.employee_id = sal.employee_id
+inner join parks_departments pd
+	on sal.dept_id = pd.department_id
+;
+select * 
+from parks_departments
+;
